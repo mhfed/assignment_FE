aea86e10e59d5052ba12da16db1e37d121531f66
Hieu Minh <nmhieu04091999@gmail.com>
2022-05-02 15:00:35 +0700

    fix: bug api user,token

--- src/api/category.tsx
+++ src/api/category.tsx
@@ -1,17 +1,17 @@
 import { CategoryType } from "../types/category";
 import instance from "./instance";
 import { getLocalStorage } from "../utils/localStorage";
-const { token, user } = getLocalStorage();
 
+const userInfo = getLocalStorage();
 export const list = () => {
     const url = "/categories";
     return instance.get(url);
 };
 export const remove = (slug: string) => {
-    const url = `/categories/${slug}/${user._id}`;
+    const url = `/categories/${slug}/${userInfo.user._id}`;
     return instance.delete(url, {
         headers: {
-            Authorization: `Bearer ${token}`,
+            Authorization: `Bearer ${userInfo.token}`,
         },
     });
 };
@@ -20,18 +20,18 @@ export const get = (slug: string | undefined) => {
     return instance.get(url);
 };
 export const add = (category: CategoryType) => {
-    const url = `/categories/${user._id}`;
+    const url = `/categories/${userInfo.user._id}`;
     return instance.post(url, category, {
         headers: {
-            Authorization: `Bearer ${token}`,
+            Authorization: `Bearer ${userInfo.token}`,
         },
     });
 };
 export const update = (product: CategoryType, id: number) => {
-    const url = `/categories/${id}/${user._id}`;
+    const url = `/categories/${id}/${userInfo.user._id}`;
     return instance.put(url, product, {
         headers: {
-            Authorization: `Bearer ${token}`,
+            Authorization: `Bearer ${userInfo.token}`,
         },
     });
 };
--- src/api/product.tsx
+++ src/api/product.tsx
@@ -1,17 +1,17 @@
 import { ProductType } from "../types/product";
 import instance from "./instance";
 import { getLocalStorage } from "../utils/localStorage";
-const { token, user } = getLocalStorage();
+const userInfo = getLocalStorage();
 
 export const list = () => {
     const url = "/products";
     return instance.get(url);
 };
 export const remove = (id: number) => {
-    const url = `/products/${id}/${user._id}`;
+    const url = `/products/${id}/${userInfo.user._id}`;
     return instance.delete(url, {
         headers: {
-            Authorization: `Bearer ${token}`,
+            Authorization: `Bearer ${userInfo.token}`,
         },
     });
 };
@@ -20,18 +20,18 @@ export const read = (id: string | undefined) => {
     return instance.get(url);
 };
 export const add = (product: ProductType) => {
-    const url = `/products/${user._id}`;
+    const url = `/products/${userInfo.user._id}`;
     return instance.post(url, product, {
         headers: {
-            Authorization: `Bearer ${token}`,
+            Authorization: `Bearer ${userInfo.token}`,
         },
     });
 };
 export const update = (product: ProductType, id: number) => {
-    const url = `/products/${id}/${user._id}`;
+    const url = `/products/${id}/${userInfo.user._id}`;
     return instance.put(url, product, {
         headers: {
-            Authorization: `Bearer ${token}`,
+            Authorization: `Bearer ${userInfo.token}`,
         },
     });
 };
